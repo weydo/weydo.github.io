b05b7ce2778deec59308c1765c4716943d938119
weydo <13163258835@163.com>
2022-02-09 10:14:14 +0800

    付费页面demo 2022年2月9日10:14:11

--- main.js
+++ main.js
@@ -2,10 +2,25 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import VueResource from 'vue-resource'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI,VueResource);
 
 new Vue({
   el: '#app',
   render: h => h(App)
 });
+
+// Vue.config.productionTip = false
+
+// App.mpType = 'app'
+
+// // 引入全局uView
+// import uView from 'uview-ui'
+// Vue.use(uView);
+// const app = new Vue({
+//     ...App
+// })
+// import httpInterceptor from '@/common/http.interceptor.js';
+// Vue.use(httpInterceptor, app);
+// app.$mount()
